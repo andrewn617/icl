e04026d7b7d589731333a89920fcb1fee6f40e6f
Andrew Novoselac <andrewn@nulogy.com>
2020-01-16 18:45:55 -0500

    update paths

--- lib/icl.rb
+++ lib/icl.rb
@@ -3,10 +3,10 @@ require 'nokogiri'
 
 module Icl
   def self.output(document_type)
-    event = File.open("#{document_type}/event.xml") { |f| Nokogiri::XML(f) }
-    transform = File.open("#{document_type}/transform_1.xml") { |f| Nokogiri::XSLT(f) }
+    event = File.open("document_types/#{document_type}/event.xml") { |f| Nokogiri::XML(f) }
+    transform = File.open("document_types/{document_type}/transform_1.xml") { |f| Nokogiri::XSLT(f) }
 
-    File.write("#{document_type}/output.xml", transform.transform(event))
+    File.write("document_types/#{document_type}/output.xml", transform.transform(event))
   end
 end
 
