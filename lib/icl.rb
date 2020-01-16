45bb6f529a5863b1378b6bfa5ee7104b84634daf
Andrew Novoselac <andrewn@nulogy.com>
2020-01-16 18:40:22 -0500

    clean up

--- lib/icl.rb
+++ lib/icl.rb
@@ -1,2 +1,12 @@
 require 'icl/version.rb'
+require 'nokogiri'
+
+module Icl
+  def self.output(document_type)
+    event = File.open("#{document_type}/event.xml") { |f| Nokogiri::XML(f) }
+    transform = File.open("#{document_type}/transform_1.xml") { |f| Nokogiri::XSLT(f) }
+
+    File.write("#{document_type}/output.xml", transform.transform(event))
+  end
+end
 
--- lib/icl/version.rb
+++ lib/icl/version.rb
@@ -1,12 +1,3 @@
-require "nokogiri"
-
 module Icl
   VERSION = '0.0.1'
-
-  def self.output(document_type)
-    event = File.open("#{document_type}/event.xml") { |f| Nokogiri::XML(f) }
-    transform = File.open("#{document_type}/transform_1.xml") { |f| Nokogiri::XSLT(f) }
-
-    File.write("#{document_type}/output.xml", transform.transform(event))
-  end
 end
