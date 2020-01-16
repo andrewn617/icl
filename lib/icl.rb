10a2ac5306c6200288b5bcc999cdd04e0fe8f38a
Andrew Novoselac <andrewn@nulogy.com>
2020-01-16 18:46:28 -0500

    fix typo

--- lib/icl.rb
+++ lib/icl.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 module Icl
   def self.output(document_type)
     event = File.open("document_types/#{document_type}/event.xml") { |f| Nokogiri::XML(f) }
-    transform = File.open("document_types/{document_type}/transform_1.xml") { |f| Nokogiri::XSLT(f) }
+    transform = File.open("document_types/#{document_type}/transform_1.xml") { |f| Nokogiri::XSLT(f) }
 
     File.write("document_types/#{document_type}/output.xml", transform.transform(event))
   end
