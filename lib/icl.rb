53cc96fb1ccf33c4b7a516a6d82c3004da18003a
Andrew Novoselac <andrewn@nulogy.com>
2020-01-16 21:35:07 -0500

    rename output to document

--- lib/icl.rb
+++ lib/icl.rb
@@ -10,11 +10,11 @@ require 'nokogiri'
 # => support multiple events per doc type
 
 module Icl
-  def self.output(document_type)
+  def self.generate(document_type)
     event = File.open("document_types/#{document_type}/event.xml") { |f| Nokogiri::XML(f) }
     transform = File.open("document_types/#{document_type}/transform_1.xml") { |f| Nokogiri::XSLT(f) }
 
-    File.write("document_types/#{document_type}/output.xml", transform.transform(event))
+    File.write("document_types/#{document_type}/document.xml", transform.transform(event))
   end
 end
 
