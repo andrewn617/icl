b1a4830fd1e6b35aaab4878d82ae962bd6ff1308
Andrew Novoselac <andrewn@nulogy.com>
2020-01-16 21:38:41 -0500

    change name of transform file

--- lib/icl.rb
+++ lib/icl.rb
@@ -12,7 +12,7 @@ require 'nokogiri'
 module Icl
   def self.generate(document_type)
     event = File.open("document_types/#{document_type}/event.xml") { |f| Nokogiri::XML(f) }
-    transform = File.open("document_types/#{document_type}/transform_1.xml") { |f| Nokogiri::XSLT(f) }
+    transform = File.open("document_types/#{document_type}/transform.xml") { |f| Nokogiri::XSLT(f) }
 
     File.write("document_types/#{document_type}/document.xml", transform.transform(event))
   end
