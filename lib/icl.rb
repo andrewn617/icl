255be0be769be0d0fc5ab06eaace2a25170d9a10
Andrew Novoselac <andrewn@nulogy.com>
2020-01-16 21:31:53 -0500

    add to do list

--- lib/icl.rb
+++ lib/icl.rb
@@ -1,6 +1,14 @@
 require 'icl/version.rb'
 require 'nokogiri'
 
+# to do
+# => add tests
+# => make the path less specific
+# => command to set up a project
+# => command to set up a document type
+# => support multiple transform per doc type
+# => support multiple events per doc type
+
 module Icl
   def self.output(document_type)
     event = File.open("document_types/#{document_type}/event.xml") { |f| Nokogiri::XML(f) }
