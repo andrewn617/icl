035148dc9ea553353d1339f400f14b4ef703cb93
Andrew Novoselac <andrewn@nulogy.com>
2020-01-17 13:54:46 -0500

    Version 1

--- lib/icl.rb
+++ lib/icl.rb
@@ -1,27 +1,17 @@
 require 'icl/version.rb'
 require 'nokogiri'
 
-# to do
-# => add tests
-# => code smells?
-# => how do i get faster feedback without publish gem
-# => make the path less specific
-# => command to set up a project
-# => command to set up a document type
-# => support multiple transform per doc type
-# => support multiple events per doc type
-
 module Icl
-  def self.transform(event_file, *stylesheet_files)
-    event_string = File.open("#{event_file}")
+  def self.transform(xml_file, *stylesheet_files)
+    xml_string = File.open("#{xml_file}")
     stylesheet_strings = stylesheet_files.map { |stylesheet_file| File.open("#{stylesheet_file}") }
 
-    puts transform_from_strings(event_string, *stylesheet_strings)
+    puts transform_from_strings(xml_string, *stylesheet_strings)
   end
 
-  def self.transform_from_strings(event_string, *stylesheet_strings)
-    event = Nokogiri::XML(event_string)
-    output = event
+  def self.transform_from_strings(xml_string, *stylesheet_strings)
+    xml = Nokogiri::XML(xml_string)
+    output = xml
 
     stylesheet_strings.each do |stylesheet_string|
       stylesheet = Nokogiri::XSLT(stylesheet_string)
