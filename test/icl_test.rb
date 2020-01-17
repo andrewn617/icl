9a98171529d37cd73d63cca71debb81343ee62d5
Andrew Novoselac <andrewn@nulogy.com>
2020-01-17 13:47:20 -0500

    add another test

--- test/icl_test.rb
+++ test/icl_test.rb
@@ -6,6 +6,7 @@ class IclTest < Minitest::Test
     event = <<~XML
       <?xml version="1.0"?>
       <name>
+        <title>Dr.</title>
         <first>John</first>
         <last>Smith</last>
       </name>
@@ -15,7 +16,10 @@ class IclTest < Minitest::Test
       <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 
         <xsl:template match="name">
-          <name><xsl:value-of select="concat(first, ' ', last)"/></name>
+          <name>
+            <xsl:copy-of select="title"/>
+            <full><xsl:value-of select="concat(first, ' ', last)"/></full>
+          </name>
         </xsl:template>
 
       </xsl:stylesheet>
@@ -23,9 +27,54 @@ class IclTest < Minitest::Test
 
     document = <<~XML
       <?xml version="1.0"?>
-      <name>John Smith</name>
+      <name>
+        <title>Dr.</title>
+        <full>John Smith</full>
+      </name>
     XML
 
     assert_equal document, Icl.generate_from_strings(event, transform).to_s
+  end
+
+  def test_transform_an_event_using_multiple_transforms
+    event = <<~XML
+      <?xml version="1.0"?>
+      <name>
+        <title>Dr.</title>
+        <first>John</first>
+        <last>Smith</last>
+      </name>
+    XML
+
+    transform_1 = <<~XSLT
+      <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+        <xsl:template match="name">
+          <name>
+            <full><xsl:value-of select="concat(first, ' ', last)"/></full>
+            <xsl:copy-of select="title"/>
+          </name>
+        </xsl:template>
+
+      </xsl:stylesheet>
+    XSLT
+
+
+    transform_2 = <<~XSLT
+      <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+        <xsl:template match="name">
+          <name><xsl:value-of select="concat(title, ' ', full)"/></name>
+        </xsl:template>
+
+      </xsl:stylesheet>
+    XSLT
+
+    document = <<~XML
+      <?xml version="1.0"?>
+      <name>Dr. John Smith</name>
+    XML
+
+    assert_equal document, Icl.generate_from_strings(event, transform_1, transform_2).to_s
   end
 end
